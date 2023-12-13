1132117c93ddbbb2b5c51a18343419cf758bc68c
kestulka <kgirdziunas0@gmail.com>
2023-12-13 09:36:50 +0200

    abc

--- pamoka.js
+++ pamoka.js
@@ -1,10 +1,10 @@
-import duomenys from "./data2.js"
+import data from "./data.json" assert { type: "json" };
 
 const patikra = localStorage.getItem("countries")
 
 const salys = patikra === null ? [] : JSON.parse(patikra)
 
-duomenys.forEach((el, index) => {
+data.forEach((el, index) => {
     if (!el.hasOwnProperty('capital')){
         const country = {
             id: index+1,
